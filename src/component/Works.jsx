9ea3799bcb5aa3270c6e4c3e7f20b7766e7a5d2f
andry06 <andri.suryono@gmail.com>
2023-10-25 08:08:35 +0700

    update portofolio

--- src/component/Works.jsx
+++ src/component/Works.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Sejam from '../assets/sejam.png';
+import sistem_produksi from '../assets/sistem_produksi.png';
 import Modal from './Modal';
 
   
@@ -37,7 +38,7 @@ const Works = () => {
             </div>
 
             <div className='w-[320px] h-[320px]  shadow-xl hover:scale-110  ease-in-out  rounded-xl' >
-                <img src={Sejam} alt="pos_food" className='w-full h-[250px]  rounded-t-lg' />
+                <img src={sistem_produksi} alt="pos_food" className='w-full h-[250px]  rounded-t-lg' />
                 
                 <div className='w-full h-[150px] bg-white dark:bg-[#04133e] shadow-2xl  border rounded-b-lg'>
                     <h4 className='text-2xl text-black dark:text-white font-semibold py-2 px-3 '>
